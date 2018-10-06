22ba987656e502d100dd95834d7a116a877c7603
Sai-Kappagantula <kappagantula4@gmail.com>
2018-10-06 00:49:17 -0400

    Login Button Finalized
    Finalized Login Buttons

--- src/components/LoginButtons/index.tsx
+++ src/components/LoginButtons/index.tsx
@@ -1,9 +1,40 @@
 import * as React from 'react';
 import Button from '../common/Button';
+type ButtonType = {
+  bgColor: string;
+  text: string;
+  onClick: () => void;
+}
+type State = {
+  buttons: ButtonType[];
+}
 
-class LoginButtons extends React.Component {
-
-
+class LoginButtons extends React.Component<{}, State> {
+  
+  state: State = {
+    buttons: []
+  }
+  componentDidMount() {
+    this.setState({
+      buttons: [
+        {
+          bgColor: 'red',
+          text: 'test',
+          onClick: this.login
+        },
+        {
+          bgColor: 'red',
+          text: 'test',
+          onClick: this.login
+        },
+        {
+          bgColor: 'red',
+          text: 'test',
+          onClick: this.login
+        }
+      ]
+    })
+  }
   login() {
     alert("Login with username and password");
   }
@@ -16,28 +47,19 @@ class LoginButtons extends React.Component {
   render() {
     return (
       <div>
-        <Button
-            bgColor="green"
-            text="Login"
-            onClick={
-              this.login
-            }
-          />
-        <Button
-          bgColor="blue"
-          text="Login with Facebook"
-          onClick={
-            this.loginWithFacebook
-          }
-        />
-        <Button 
-          bgColor="red"
-          text="Login with Google"
-          onClick={
-            this.loginWithGoogle
-          }
-        
-        />
+
+        {
+          this.state.buttons.map((button, i) => {
+            return(
+              <Button
+                bgColor={button.bgColor}
+                text={button.text}
+                onClick={button.onClick}
+                key={i}
+              />
+            );            
+          })
+        }
       </div>
     );
   }
